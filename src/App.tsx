8e385f54b6ed0b026c4461df9c40216fc25ac0e4
ayubh21 <ayubhussein625@gmail.com>
2025-07-25 05:39:41 -0400

    changed resume

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import MysqlLogo from "../src/assets/mysql.svg"
 import DotnetLogo  from "../src/assets/dotnet.svg"
 import AWSLogo from "../src/assets/aws.svg"
 import PostgresLogo from "../src/assets/postgresql.svg"
-import ResumeFile from "../src/assets/Jake_s_Resume__4_.pdf"
+import ResumeFile from "../src/assets/ayubs_resume.pdf"
 import ProjectCard from './components/recent-work-card'
 import {motion} from "framer-motion"
 import LinkedinInIcon from "../src/assets/linkedin-svgrepo-com.svg"
